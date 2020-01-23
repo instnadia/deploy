f804ff78ad4c7b7a37990cac75253c776f774bd8
instnadia <nnguyen@codingdojo.com>
2020-01-23 12:42:27 -0800

    project

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+# NO LEADING SLASHES
+urlpatterns = [
+    path('', views.index), #GET
+    path('register', views.register), #POST
+    path('success', views.success), #GET
+    path('logout', views.logout), #GET
+    path('login', views.login), #POST
+    path('new_message', views.new_message), #POST
+    path('new_comment/<int:id>', views.new_comment), #POST
+    path('message/<int:id>/delete', views.delete_message), #POST
+    path('comment/<int:id>/delete', views.delete_comment)
+]
